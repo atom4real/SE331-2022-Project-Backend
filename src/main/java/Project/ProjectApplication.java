990b2db447c53b116deb9f5242bb91b95072eb18
atomfaster@gmail.com <atomfaster@gmail.com>
2022-11-01 12:03:16 +0700

    deploy allow cors

--- src/main/java/Project/ProjectApplication.java
+++ src/main/java/Project/ProjectApplication.java
@@ -23,7 +23,7 @@ public class ProjectApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","http://127.0.0.1:3000/","http://54.174.246.47")
+                        .allowedOrigins("http://localhost:3000","http://127.0.0.1:3000/","http://18.234.126.238")
                         .exposedHeaders("x-total-count");
             }
         };
